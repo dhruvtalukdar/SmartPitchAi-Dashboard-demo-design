d02a30be8f63bdad02d046e3232cebae9c3ee89b
Dhrubajyoti Talukdar <95336679+dhruvtalukdar@users.noreply.github.com>
2025-06-19 12:13:19 +0530

    Fix Vite configuration for ES modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import path from 'path';
+import { fileURLToPath, URL } from 'node:url';
+import * as path from 'node:path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
